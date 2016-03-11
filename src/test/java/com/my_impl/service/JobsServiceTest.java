6acd99ac31f2e55f67278e34ac598f737502889c
Tiberiu Adrian CARP <tcarp@localhost.localdomain>
2016-03-11 20:26:33 +0100

    Completing JobsServiceTest

--- src/test/java/com/my_impl/service/JobsServiceTest.java
+++ src/test/java/com/my_impl/service/JobsServiceTest.java
@@ -30,13 +30,13 @@ public class JobsServiceTest {
 		
 		job2 = new Job
 				.JobBuilder()
-				.withNetAmount(new BigDecimal(100.0))
-				.withVatRate(new BigDecimal(20.0))
+				.withVatAmount(new BigDecimal(247.0))
+				.withVatRate(new BigDecimal(12.0))
 				.build();
 		
 		job3 = new Job
 				.JobBuilder()
-				.withNetAmount(new BigDecimal(100.0))
+				.withGrossAmount(new BigDecimal(100.0))
 				.withVatRate(new BigDecimal(20.0))
 				.build();
 		
@@ -50,17 +50,17 @@ public class JobsServiceTest {
 	
 		job2Result = new Job
 				.JobBuilder()
-				.withNetAmount(new BigDecimal(100.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
-				.withVatAmount(new BigDecimal(20.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
-				.withGrossAmount(new BigDecimal(120.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
-				.withVatRate(new BigDecimal(20.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
+				.withNetAmount(new BigDecimal(2058.3333).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
+				.withVatAmount(new BigDecimal(247.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
+				.withGrossAmount(new BigDecimal(2305.3333).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
+				.withVatRate(new BigDecimal(12.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
 				.build();
 	
 		job3Result = new Job
 				.JobBuilder()
-				.withNetAmount(new BigDecimal(100.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
-				.withVatAmount(new BigDecimal(20.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
-				.withGrossAmount(new BigDecimal(120.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
+				.withNetAmount(new BigDecimal(83.3333).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
+				.withVatAmount(new BigDecimal(16.6667).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
+				.withGrossAmount(new BigDecimal(100.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
 				.withVatRate(new BigDecimal(20.0).setScale(Job.NUMBER_OF_DECIMALS, Job.DEFAULT_ROUNDING_MODE))
 				.build();
 	}
@@ -72,25 +72,8 @@ public class JobsServiceTest {
 	
 	@Test
 	public void testComputeValuesForMultipleJobsShouldComplete() {
-		List<Job> resultList = jobsService.computeValuesForMultipleJobs(Arrays.asList(new Job[]{job1/*, job2, job3*/}));
-		List<Job> expectedResultList = Arrays.asList(new Job[]{job1Result/*, job2Result, job3Result*/});
-		
-		
-		//System.out.println();
-		//Assert.isTrue(resultList.equals(expectedResultList));
-		
-		//System.out.println("GrossAmount " + resultList.get(0).getGrossAmount() + ", " + expectedResultList.get(0).getGrossAmount());
-		
-		//Assert.isTrue(resultList.get(0).getGrossAmount().equals(expectedResultList.get(0).getGrossAmount()));
-		//Assert.isTrue(resultList.get(0).getNetAmount().equals(expectedResultList.get(0).getNetAmount()));
-		//Assert.isTrue(resultList.get(0).getVatAmount().equals(expectedResultList.get(0).getVatAmount()));
-		//Assert.isTrue(resultList.get(0).getVatRate().equals(expectedResultList.get(0).getVatRate()));
-		
-		/*System.out.println(resultList.get(0).getVatAmount());
-		System.out.println(expectedResultList.get(0).getVatAmount());
-		
-		System.out.println(resultList.get(0).getVatRate());
-		System.out.println(expectedResultList.get(0).getVatRate());*/
+		List<Job> resultList = jobsService.computeValuesForMultipleJobs(Arrays.asList(new Job[]{job1, job2, job3}));
+		List<Job> expectedResultList = Arrays.asList(new Job[]{job1Result, job2Result, job3Result});
 		
 		Assert.isTrue(resultList.equals(expectedResultList));
 	}
